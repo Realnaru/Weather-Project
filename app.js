0d1d5f87553b645e5c1181871c5cd229b0ff90d7
Ruslan Opishniak <73569436+Realnaru@users.noreply.github.com>
2021-04-10 00:28:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,11 +13,9 @@ res.sendFile(__dirname + "/index.html");
 
 app.post("/", function(req, res) {
 
-
   const query = req.body.cityName;
   const apiId = "2055374b5b8bb9e995396b3429f22d8b";
   const units = "metric";
-
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiId + "&units=" + units;
 
   https.get(url, function(response){
@@ -39,9 +37,6 @@ app.post("/", function(req, res) {
   });
 
 });
-
-
-
 app.listen(3000, function(){
   console.log("server is running on port 3000");
 });
